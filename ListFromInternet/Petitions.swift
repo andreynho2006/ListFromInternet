1f52a32d9b2fbc1416d54a3c3c85abaf4c525d4e
Andrei Cirlan <andreynho2006@gmail.com>
2020-11-18 10:50:10 +0200

    make another UIBarButtonItem

---
+++ ListFromInternet/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  ListFromInternet
+//
+//  Created by Andrei Cirlan on 18.11.2020.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
